f293446fdb4924bf8ce88a349406fdb38d3d7eff
Kanji Hara <kanjih@ciandt.com>
2019-09-17 15:59:38 -0700

    DONE

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom'
 import { history } from '../../helpers/history';
 import Dashboard from '../Dashboard/Dashboard'
